23be896ae356172657e41e3d8893efe3e52aadad
Damian Suarez <damian.suarez@xifox.net>
2010-12-03 13:30:19 -0300

    several tree modificafions. Bulding API and twins exampleç

--- examples/twins/app.js
+++ examples/twins/app.js
@@ -1,43 +1,24 @@
-
 // Expose modules in ./support for demo purposes
 require.paths.unshift(__dirname + '/../../support');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
+/*** Module dependencies. ***/
+var express = require('express')
+  ,  drawback = require(__dirname + '/../../lib/drawback');
 
 // Path to our public directory
-
 var pub = __dirname + '/public';
 
-// Auto-compile sass to css with "compiler"
-// and then serve with connect's staticProvider
-
 var app = express.createServer(
-  express.compiler({ src: pub, enable: ['sass'] }),
+  express.compiler({src: pub, enable: ['sass']}),
   express.staticProvider(pub)
 );
 
-// Optional since express defaults to CWD/views
-
 app.set('views', __dirname + '/views');
-
-// Set our default template engine to "jade"
-// which prevents the need for extensions
-// (although you can still mix and match)
 app.set('view engine', 'jade');
 
-// Dummy users
-var users = [
-    { name: 'tj', email: 'tj@vision-media.ca' }
-  , { name: 'ciaran', email: 'ciaranj@gmail.com' }
-  , { name: 'aaron', email: 'aaron.heckmann+github@gmail.com' }
-];
 
 app.get('/', function(req, res){
-  res.render('user/list', { users: users });
+  res.render('partials/chart', {});
 });
 
 app.listen(3000);
---
+++ examples/twins/public/js/draw/twins.js
@@ -0,0 +1,3 @@
+Drawback.register('twins', function($){
+  Drawback.draw('#some_element', 'my_example', '/data/url', options);
+});
