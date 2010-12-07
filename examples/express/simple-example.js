86e7719357b5cc68362fcc5f9bcf7692f809efd2
Tj Holowaychuk <tj@vision-media.ca>
2010-12-07 15:48:55 -0800

    Using res.attachment()

--- examples/express/simple-example.js
+++ examples/express/simple-example.js
@@ -66,10 +66,7 @@ app.get('/draw/:module_name', function(req, res){
             var header = {};
             
             if(forceDownload) {
-              header = {
-                'Content-Type': 'application/force-download name="' + modname + '.png"',
-                'Content-Disposition': 'inline; filename="' + modname + '.png"'
-              }
+              res.attachment(modname);
             }
             else header = {
               'Content-Type': 'image/png'
